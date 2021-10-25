72efa89e6dbb30fe94ed4b0213a0d9bc653ed475
mvpzlegna <mvpzlegna@gmail.com>
2021-10-25 20:41:00 +0700

    test
    test

--- config.js
+++ config.js
@@ -2,14 +2,14 @@ const { Guild } = require("discord.js");
 
 module.exports = {
     //all fields are **required**
-    token: 'OTAyMTUxODcwMTYyODIxMTYx.YXaQZw.mv9Vxp-nUVm8T4rkIkASSpauBIs', // Your bot's token
+    token: 'NzIxMzU0NzE5NzQ2MDY0NDQ1.XuTT7w.xACOiPiylIr4OaK8YNbt4gWxf8A', // Your bot's token
     position:0,
     //clockchannel: '836102276124508171', // ID of a voice channel that used to display the time
     timezone: 'Asia/Seoul', // Timezone (take a look at https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List)
     format: 'HH:mm (z)', // Clock format, leave this default seting for 24h format, read more at https://momentjs.com/docs/#/displaying/format/
     updateinterval: 600000, // Discord is ratelimiting us for 10 minutes!
     bitrate: 73643,
-    guild_id:"747108016230367332",
+    guild_id:"721291694196391947",
     userLimit:83
     //[ON WORK, IGNORE THIS FIELD] dev: 'YOUR ID', // Developer's ID for sending the errors
   }
